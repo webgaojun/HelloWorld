a54f116079ecb013ea760b647bbac06ae632e4d6
gaojun.zhou <274410087@qq.com>
2017-03-01 15:31:08 +0800

    初次提交

---
+++ src/com/vacomall/com/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.vacomall.com;
+
+/**
+ * Created by Gaojun.Zhou on 2017-03-01.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+}
